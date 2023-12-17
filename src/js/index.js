109e129ef802421c39ec7a418f0b43c2a189f30e
Daniel <Danielasratab@gmail.com>
2023-12-17 21:44:00 +0300

    Bulma Framework

--- src/js/index.js
+++ src/js/index.js
@@ -11,29 +11,42 @@ burgerIcon.addEventListener('click', () =>{
     navbarMenu.classList.toggle('is-active');
 });
 
-  var myButton = document.getElementById('button_contact');
+  var ContactButton = document.getElementById('button_contact');
+  var Hire_me = document.getElementById('Hire_me');
 
-  myButton.addEventListener('click', function() {
-      var filePath = './contact-me.html';
+
+  ContactButton.addEventListener('click', function() {
+      var filePath = './Contact-me.html';
       window.location.href = filePath;
   });
 
+  Hire_me.addEventListener('click', function() {
+    var filePath = './Contact-me.html';
+    window.location.href = filePath;
+  });
+
+  function downloadCv(){
+    var filePath = './DanielAsratCV.pdf'
+    window.open(filePath)
+  }
+
+
   Facebook.addEventListener('click', function() {
-    location.href = "https://www.facebook.com";
+    location.href = "https://www.facebook.com/profile.php?id=100079797836024";
 
   })
 
   Instagram.addEventListener('click', function() {
-    location.href = "https://www.instagram.com";
+    location.href = "https://www.instagram.com/_danielasrat/";
     
   })
 
   Youtube.addEventListener('click', function() {
-    location.href = "https://www.youtube.com";
+    location.href = "https://www.youtube.com/@ADTech_Tube";
     
   })
 
   Linkedin.addEventListener('click', function() {
-    location.href = "https://www.linkedin.com";
+    location.href = "https://www.linkedin.com/in/danielasrat/";
     
   })
